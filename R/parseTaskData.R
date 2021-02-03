6e6149b91a499ee6d6c97e91055fe89bcd62636e
thartbm <thartbm@gmail.com>
2021-02-02 22:14:00 -0500

    generic parse function for pavlovia data

--- R/parseTaskData.R
+++ R/parseTaskData.R
@@ -5,22 +5,57 @@
 
 getGroupPerformance <- function(year, semester, task) {
   
-  # get list of participants / file names
-  
-  
-  
+  # load function and settings from task source file
   source(paste0('R/',task,'.R'))
   
-  print(usefirst)
+  # "settings" are variables declared in the sourced file:
+  # nlines: a vector of acceptable number fo lines in each csv file
+  # usefirst: boolean saying if we use the first of multiple files for one participants
+  # (usually best to keep this TRUE)
   
-  # now we can use do.call, to run a named function on some data:
+  # get list of file names
+  folder <- sprintf('data/%s/%s/%s/',year,semester,task)
+  files <- list.files(folder,pattern='*.csv')
   
-  # do.call(task, DF)
+  print(length(files))
   
-  # f <- match.fun(task)
+  # use readLines and weed out those with too few lines
+  filelines <- unlist(lapply(sprintf('data/%s/%s/%s/%s',year,semester,task,files), function(x){length(readLines(x))}))
+  files <- files[which(filelines %in% nlines)]
   
-  # and use lapply to run stuff on all participants with simplify = 'matrix' ?
+  # extract participant IDs and timestamps
+  participants <- as.data.frame(do.call("rbind", lapply(files, getIDtimestamp, 'visualsearch')), stringsAsFactors=F)
+  participants <- participants[order(participants$timestamp),]
+  row.names(participants) <- NULL
+  
+  # remove duplicates:
+  participants <- participants[!duplicated(participants$participant, fromLast=!usefirst),]
+  
+  # get relative filenames:
+  participants$filename <- sprintf('data/%s/%s/%s/%s_visualsearch_%s.csv',year,semester,task,participants$participant,participants$timestamp)
+  
+  # magic: this assigns a function to f, by finding a function
+  # that has the name specified in the character variable task
+  # which is why the function in the sourced file
+  # (that has the same name as the task)
+  # needs to have the same name as the task
+  f <- match.fun(task)
+  
+  # and use lapply to run stuff on all participants
+  functionoutput <- as.data.frame(do.call("rbind", lapply(participants$filename, f)))
   
   # return a data frame
+  
+}
+
+getIDtimestamp <- function(filename, task) {
+  
+  pattern <- sprintf('_%s_', task)
+  
+  pos <- gregexpr(pattern=pattern, filename)[[1]][1]
+  pp <- substr(filename, 1, pos-1)
+  ts <- substr(filename, pos+nchar(pattern), nchar(filename)-4)
+  
+  return(c('participant'=pp, 'timestamp'=ts))
   
 }
--- R/visualsearch.R
+++ R/visualsearch.R
@@ -1,8 +1,25 @@
 
-usefirst <- FALSE
+# use the first (or alternatively the last) of several files for the same participant?
+usefirst <- TRUE
 
-visualsearch <- function(df) {
+# how many lines should be in a data file? (there may be multiple correct options)
+nlines <- c(165)
+
+visualsearch <- function(filename) {
+  
+  # first we read the data file:
+  df <- read.csv(filename, stringsAsFactors=F)
+  
+  # remove lines for breaks:
+  df[which(!is.na(df$trialResp.corr)),]
   
   
+  
+  # we remove the trials without a correct response:
+  df[which(df$trialResp.corr == 1),]
+  
+  
+  
+  return(c('rows'=dim(df)[1],'cols'=dim(df)[2]))
   
 }
