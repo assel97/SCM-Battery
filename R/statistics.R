dd1295b483aa7f7dd6e121d4c9d1f9905b322208
thartbm <thartbm@gmail.com>
2021-02-03 19:50:41 -0500

    add file with statistics functions

---
+++ R/statistics.R
@@ -0,0 +1,39 @@
+
+
+# Hautus correction:
+# add 0.5 to both the number of hits and the number of false alarms, 
+# and add 1 to both the number of signal trials and the number of noise trials;
+# dubbed the loglinear approach (Hautus, 1995)
+
+dprime <- function(hits, misses, fas, crs, hautus=FALSE) {
+  
+  if (hautus) {
+    hit_rate <- (hits + 0.5) / (hits + misses + 1)
+    fa_rate  <- (fas  + 0.5) / (fas  + crs    + 1)
+  } else {
+    hit_rate <- hits / (hits + misses)
+    fa_rate  <- fas  / (fas  + crs)
+  }
+  
+  Zhr    <- qnorm(hit_rate)
+  Zfr    <- qnorm(fa_rate)
+  dprime <- Zhr - Zfr
+  beta   <- exp(-Zhr * Zhr/2 + Zfr * Zfr/2)
+  c      <- -(Zhr + Zfr)/2
+  
+  return( list( 'dprime' = dprime,
+                'beta'   = beta,
+                'c'      = c         ))
+  
+}
+
+cowank <- function(hits, misses, fas, crs, N) {
+  
+  hit_rate <- hits / (hits + misses)
+  cr_rate  <- crs  / (crs  + fas)
+  
+  K <- (hit_rate + cr_rate - 1) * N
+  
+  return(K)
+  
+}
